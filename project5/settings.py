3032273d318e7101f5c15656b3db5df0c7e42264
欧超超 <528422793@qq.com>
2020-06-21 18:08:38 +0800

    add serializers

--- project5/settings.py
+++ project5/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
+    'dj',
     'api',
 ]
 
@@ -122,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
--- project5/urls.py
+++ project5/urls.py
@@ -1,13 +1,26 @@
-from django.conf.urls import url
-from api import views
+from django.conf.urls import url, include
+from api import views as api_views
+from dj import views as dj_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', dj_views.UserViewSet)
+router.register(r'groups', dj_views.GroupViewSet)
 
 urlpatterns = [
     # 用户登录接口
-    url(r'login/$', views.LoginView.as_view()),
+    url(r'login/$', api_views.LoginView.as_view()),
 
     # 商品列表接口
-    url(r'goods/$', views.GoodsView.as_view()),
+    url(r'goods/$', api_views.GoodsView.as_view()),
 
     # 用户信息接口
-    url(r'userinfo/$', views.UserInfoView.as_view()),
+    url(r'userinfo/$', api_views.UserInfoView.as_view()),
+
+    # 使用自动URL路由连接我们的API。
+    # 另外，我们还包括支持浏览器浏览API的登录URL。
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include('snippets.urls')),
+
 ]
